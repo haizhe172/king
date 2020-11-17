1346f5891ab9938a24430b0af4afc11615d24b87
xu <1991972116@qq.com>
2020-11-17 08:37:10 +0800

    add3

--- resources/views/admin/seckill/index.blade.php
+++ resources/views/admin/seckill/index.blade.php
@@ -24,7 +24,7 @@
   <!-- .box-body -->
 
                     <div class="box-header with-border">
-                        <h3 class="box-title">广告列表</h3>
+                        <h3 class="box-title">秒杀商品信息列表</h3>
                     </div>
 
                     <div class="box-body">
@@ -32,14 +32,14 @@
                         <!-- 数据表格 -->
                         <div class="table-box">
 
-                            <div class="box-tools pull-right">
+                            <!-- <div class="box-tools pull-right">
                                 <div class="has-feedback">
-                                    <form action="/admin/ad/index" method="get">
+                                    <form action="/admin/seckill/index" method="get">
 							            <input type="text" placeholder="请输入广告名称..." name="ad_name" value="{{$ad_name??''}}">
 									<button type="submit" class="btn btn-default" >搜索</button>
                                     </form>
                                 </div>
-                            </div>
+                            </div> -->
 
 			                  <!--数据列表-->
 			                  <table id="dataList" class="table table-bordered table-striped table-hover dataTable">
@@ -48,32 +48,31 @@
 			                              <th class="" style="padding-right:0px">
 			                                  <input id="selall" type="checkbox" class="icheckbox_square-blue">
 			                              </th>
-                                    <th class="sorting_asc">广告id</th>
-                                      <th class="sorting">广告名称</th>
-                                      <th class="sorting">媒介类型</th>
-                                          <th class="sorting">广告位置</th>
-									                    <th class="sorting">开始时间</th>
+                                          <th class="sorting_asc">id</th>
+                                          <th class="sorting">秒杀活动名称</th>
+                                          <th class="sorting">商品id</th>
+                                          <th class="sorting">商品库存</th>
+									                        <th class="sorting">开始时间</th>
                                           <th class="sorting">结束时间</th>
-                                          <th class="sorting">广告链接</th>
-                                          <th class="sorting">广告图片</th>
-                                          <th class="sorting">图片网址</th>
+                                          <th class="sorting">创建时间</th>
+                                          <th class="sorting">秒杀活动介绍</th>
                                           <th class="sorting">是否开启</th>
 					                      <th class="text-center">操作</th>
 			                          </tr>
 			                      </thead>
 			                      <tbody>
-                                      @foreach($ad as $v)
+                                      @foreach($seckill as $v)
 			                          <tr>
                                           <td><input name="selall[]"  type="checkbox"></td>
-				                          <td>{{$v->ad_id}}</td>
-									      <td attr_id="{{$v->ad_id}}">
-                                            <span class="span_name">{{$v->ad_name}}</span>
+                                          <td>{{$v->seckill_id}}</td>
+                                          <td attr_id="{{$v->seckill_name}}">
+                                            <span class="span_name">{{$v->seckill_name}}</span>
                                           </td>
-                                          <td attr_id="{{$v->template_id}}">
-                                            <span class="span_name">{{$v->template_name}}</span>
+                                          <td attr_id="{{$v->goods_id}}">
+                                            <span class="span_name">{{$v->goods_name}}</span>
                                           </td>
-                                          <td attr_id="{{$v->position_id}}">
-                                            <span class="span_name">{{$v->position_name}}</span>
+                                          <td attr_id="{{$v->goods_num}}">
+                                            <span class="span_name">{{$v->goods_num}}</span>
                                           </td>
                                           <td attr_id="{{$v->start_time}}">
                                             <span class="span_name">{{date('Y-m-d',$v->start_time)}}</span>
@@ -81,29 +80,26 @@
                                           <td attr_id="{{$v->end_time}}">
                                             <span class="span_name">{{date('Y-m-d',$v->end_time)}}</span>
                                           </td>
-                                          <td attr_id="{{$v->ad_link}}">
-                                            <span class="span_name">{{$v->ad_link}}</span>
+                                          <td attr_id="{{$v->create_time}}">
+                                            <span class="span_name">{{date('Y-m-d',$v->create_time)}}</span>
                                           </td>
-                                          <td attr_id="{{$v->ad_image}}">
-                                            <span class="span_name">@if($v->ad_image)<img src="{{$v->ad_image}}" width="60"> @endif</span>
-                                          </td>
-                                          <td attr_id="{{$v->image_url}}">
-                                            <span class="span_name">{{$v->image_url}}</span>
+                                          <td attr_id="{{$v->seckill_desc}}">
+                                            <span class="span_name">{{$v->seckill_desc}}</span>
                                           </td>
                                           <td attr_id="{{$v->is_on}}">
                                             <span class="span_name">@if($v->is_on==1)开启 @elseif ($v->is_on==2)关闭 @endif</span>
                                           </td>
                                           
 		                                  <td class="text-center">
-		                                 	  <a href="{{url('/admin/ad/edit/'.$v->ad_id)}}" class="btn bg-olive btn-xs">修改</a>
-		                                 	  <a href="javascript:;" class="btn bg-olive btn-xs" onclick="DeleteGetId({{$v->ad_id}},this)">删除</a>
+		                                 	  <a href="{{url('/admin/seckill/edit/'.$v->seckill_id)}}" class="btn bg-olive btn-xs">修改</a>
+		                                 	  <a href="javascript:;" class="btn bg-olive btn-xs" onclick="DeleteGetId({{$v->seckill_id}},this)">删除</a>
 											  <!-- <button class="btn btn-default" ng-click="goListPage()"><a href="#">添加广告</a></button> -->
 		                                   </td>
 			                          </tr>
                                       @endforeach
                                       <tr>
                                         <button type="button" class="btn bg-olive btn-xs delall">批量删除</button>
-                                        <td colspan="12" align="center">{{$ad->appends(['ad_name'=>$ad_name])->links()}}</td>
+                                        <td colspan="12" align="center">{{$seckill->links()}}</td>
                                       </tr>
 			                      </tbody>
 			                  </table>
@@ -136,7 +132,7 @@
     //ajax删除
     function DeleteGetId(id,obj){
         // alert(id)
-        $.get('/admin/ad/del/'+id,function(res){
+        $.get('/admin/seckill/del/'+id,function(res){
             if(res.code==00000){
                 alert(res.msg);
                 location.reload();
@@ -144,18 +140,18 @@
         },'json')
     }
 
-    // 全选反选
-    $('#selall:eq(0)').click(function(){
-        // alert(123)
-        var checkval = $("#selall").prop('checked');
-        // alert(checkval);
-        $('input[name="selall[]"]').prop('checked',checkval);
-        // if(checkval){
-        //     $('input[name="selall[]"]').addClass('checked');
-        // }else{
-        //     $('input[name="selall[]"]').removeClass('checked');
-        // }
-    })
+//     // 全选反选
+//     $('#selall:eq(0)').click(function(){
+//         // alert(123)
+//         var checkval = $("#selall").prop('checked');
+//         // alert(checkval);
+//         $('input[name="selall[]"]').prop('checked',checkval);
+//         // if(checkval){
+//         //     $('input[name="selall[]"]').addClass('checked');
+//         // }else{
+//         //     $('input[name="selall[]"]').removeClass('checked');
+//         // }
+//     })
 //     // 全删
 //     $('.delall').click(function(){
 //         // alert(123);
